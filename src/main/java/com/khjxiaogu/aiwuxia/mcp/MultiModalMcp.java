114ce525eae04964e081cf9ae2b709a0e92f76bc
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2026-05-27 15:29:08 +0800

    add usage

--- src/main/java/com/khjxiaogu/aiwuxia/mcp/MultiModalMcp.java
+++ src/main/java/com/khjxiaogu/aiwuxia/mcp/MultiModalMcp.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.khjxiaogu.aiwuxia.llm.AIOutput;
 import com.khjxiaogu.aiwuxia.llm.AIRequest;
 import com.khjxiaogu.aiwuxia.llm.DirectHistoryItem;
 import com.khjxiaogu.aiwuxia.llm.LLMConnector;
@@ -16,11 +17,12 @@ import com.khjxiaogu.aiwuxia.llm.message.ImageContent;
 import com.khjxiaogu.aiwuxia.llm.message.MessageContents;
 import com.khjxiaogu.aiwuxia.objectstorage.ObjectStorageProvider;
 import com.khjxiaogu.aiwuxia.state.Role;
+import com.khjxiaogu.aiwuxia.state.session.AISession;
 import com.khjxiaogu.aiwuxia.utils.FileUtil;
 import com.khjxiaogu.aiwuxia.utils.MCPTools;
 
 public class MultiModalMcp {
-	public static MCPTools create(ObjectStorageProvider tos) {
+	public static MCPTools create(ObjectStorageProvider tos,AISession session) {
 		MCPTools tools=new MCPTools();
 		tools.register(new ToolData.Builder("image_recognition", "使用多模态模型查看图片并返回图片描述。该功能仅负责描述图片内容，注意需要甄别描述中的内容是否合理，是否符合人物设定，人物是否应该知道对应内容。描述中的分析仅供参考，你需要独立判断是否正确。")
 				.putParam("picture_id", "72位16进制的图片id，只包含图片id本身，不得包含任何其他内容").tool((data) -> {
@@ -28,7 +30,7 @@ public class MultiModalMcp {
 					Matcher mtch = patt.matcher(data);
 					if (mtch.find()) {
 						String id = mtch.group(1);
-						return recognizeImage(tos,id);
+						return recognizeImage(tos,id,session);
 
 					}
 					return "参数格式错误";
@@ -36,17 +38,19 @@ public class MultiModalMcp {
 		return tools;
 	}
 
-	public static String recognizeImage(ObjectStorageProvider tos,String id) {
+	public static String recognizeImage(ObjectStorageProvider tos,String id,AISession session) {
 		if (tos.exists(id)) {
-			Builder builder = AIRequest.builder("picutreTool").taskType(TaskType.STORY)
+			Builder builder = AIRequest.builder(session).taskType(TaskType.STORY)
 					.multimodal(MultimodalType.IMAGE_ONLY);
 			builder.addHistoryItem(Role.SYSTEM,
-					"请观察图片，详细具体客观描述其中的内容，文字，人物，细节特征，位置等信息，并原样提供图片中所有文本原文内容。并对其所属作品、人物和表达内容进行一定的分析。");
+					"请观察图片，详细具体客观描述其中的内容，文字，人物，细节特征，位置等信息，并原样提供图片中所有文本原文内容。注意仅忠实描述图片外观，禁止进行分析。");
 			builder.addHistoryItem(
 					new DirectHistoryItem(Role.USER, new MessageContents(new ImageContent(tos.getUrl(id)))));
 			try {
+				AIOutput output=LLMConnector.call(builder.temperature(1.3f).maxTokens(3000).build());
+				output.addUsageListener(session::addUsage);
 				return FileUtil.printAndCollectContent(
-						LLMConnector.call(builder.temperature(1.3f).maxTokens(3000).build()).getContent());
+					output.getContent());
 			} catch (ModelRouteException | IOException e) {
 				e.printStackTrace();
 				return "服务暂不可用";
